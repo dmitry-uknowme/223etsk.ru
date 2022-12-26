e405e4e704ba1e10120a39fccb1f889e9bf201c1
dmitry-uknowme <bogatyrov200488@gmail.com>
2022-12-26 10:49:57 +0500

    remove favicon, fix config

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM node:lts as base
 WORKDIR /app
 COPY package*.json /
 COPY yarn.lock /
-EXPOSE 3000
+EXPOSE 3050
 
 
 FROM base as prod
@@ -14,8 +14,7 @@ COPY . /
 CMD ["yarn", "prod"]
 
 FROM base as dev
-# RUN yarn 
+RUN yarn 
 ENV NODE_ENV=development
-# RUN yarn
 COPY . /
 CMD ["yarn", "dev"]
--- renderer/_default.page.server.tsx
+++ renderer/_default.page.server.tsx
@@ -37,11 +37,12 @@ async function render(pageContext: PageContextBuiltIn & PageContext) {
   const title = (documentProps && documentProps.title) || "ЭТП Новорос-ТОРГИ";
   const desc = (documentProps && documentProps.description) || "ЭТП Новорос-ТОРГИ";
 
+  // <link rel="icon" href="${logoUrl}" />
+
   const documentHtml = escapeInject`<!DOCTYPE html>
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <link rel="icon" href="${logoUrl}" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
         <title>${title}</title>
